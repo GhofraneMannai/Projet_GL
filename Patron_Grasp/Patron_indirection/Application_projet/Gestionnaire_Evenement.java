b2d781d6ed66183d9bbd2364227fe4133ec77f19
GhofraneMannai <ghofranemanai2019@gmail.com>
2024-05-26 13:21:36 +0100

    26-05-2024

---
+++ Patron_Grasp/Patron_indirection/Application_projet/Gestionnaire_Evenement.java
@@ -0,0 +1,5 @@
+package Patron_Grasp.Patron_indirection.Application_projet;
+
+public class Gestionnaire_Evenement {
+
+}
--- PrincipeSolide/Single Responsibility Principle/Evenement.java
+++ PrincipeSolide/Single Responsibility Principle/Evenement.java
@@ -3,39 +3,17 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Evenement {
-    private Integer idEvenement;
-    private String nomEvenement;
-    private String typeEvenement;
+
+    private String nomÉvénement;
     private String description;
-    private Date dateDebut;
-    private Double prix;
-    private List<String> programme;
-    private List<String> notes;
-    private Integer placesDisponibles;
+    private String typeÉvénement;
+    private Date date;
+    private int période; // En jours
+    private double prix;
+    private int placesDisponibles;
+    private String image;
+    private List<Note> notes;
 
-    // Constructeur
-    public Evenement(Integer idEvenement, String nomEvenement, String typeEvenement, String description, 
-                     Date dateDebut, Double prix, ArrayList<String> programme, ArrayList<String> notes, Integer placesDisponibles) {
-        this.idEvenement = idEvenement;
-        this.nomEvenement = nomEvenement;
-        this.typeEvenement = typeEvenement;
-        this.description = description;
-        this.dateDebut = dateDebut;
-        this.prix = prix;
-        this.programme = programme;
-        this.notes = notes;
-        this.placesDisponibles = placesDisponibles;
-    }
 
-    // Getters et Setters
-    public Integer getIdEvenement() { return idEvenement; }
-    public String getNomEvenement() { return nomEvenement; }
-    public String getTypeEvenement() { return typeEvenement; }
-    public String getDescription() { return description; }
-    public Date getDateDebut() { return dateDebut; }
-    public Double getPrix() { return prix; }
-    public List<String> getProgramme() { return programme; }
-    public List<String> getNotes() { return notes; }
-    public Integer getPlacesDisponibles() { return placesDisponibles; }
-    public void setPlacesDisponibles(Integer placesDisponibles) { this.placesDisponibles = placesDisponibles; }
+
 }
